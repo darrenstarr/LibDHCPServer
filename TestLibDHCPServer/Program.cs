c506146280668553377157b7ad14a4f0ed92262c
Darren Starr <darren@nocturnal.no>
2017-10-17 12:15:33 +0200

    Add project files.

---
+++ TestLibDHCPServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLibDHCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
